843a68fe24cd0373d8dcaa2d23ef93fb157b4f0b
derekgilwa <dgliwa@thoughtworks.com>
2015-06-21 02:18:37 -0500

    heroku

--- app/services/comment_service.rb
+++ app/services/comment_service.rb
@@ -5,7 +5,7 @@ class CommentService
     request = HTTPI::Request.new
     request.headers['Content-Type'] = 'application/json'
     request.url = "https://api.us1.corvisa.io/sms/"
-    request.body = '[{"from_number": "+14143235967","messages": [{"text":  "' + message + '","to_number": "+1' + receiver.phone_number + '" }]}]'
+    request.body = '[{"from_number": "+14143235967","messages": [{"text":  "' + message + '", callback_url: http://beer-snob.herokuapp.com/sms_callback, "to_number": "+1' + receiver.phone_number + '" }]}]'
     request.auth.basic("hXv5b8yak2J7otSqMz3cZ", "sZeDjIShpkya.E9fQrMHLBT8w$JKC/t=2")
 
     binding.pry
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :beers
   post '/comments/', to: 'comments#create'
+  post '/sms_callback/', to: 'comments#callback'
   devise_for :users
 
   root 'application#index'
