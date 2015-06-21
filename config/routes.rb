8beeedbdf1475c233f151903330f8ce12f370300
derekgilwa <dgliwa@thoughtworks.com>
2015-06-21 03:23:48 -0500

    heroku

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :beers
   post '/comments/', to: 'comments#create'
   post '/sms_callback/', to: 'comments#callback'
-  devise_for :users
+  devise_for :users, { :registrations => 'registrations' }
 
   root 'application#index'
 end
